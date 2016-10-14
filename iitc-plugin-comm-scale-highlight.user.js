977f22803dbfb78b0e32f92fb45ed1eec76bb788
Philippe Houdoin <philippe.houdoin.ext@orange.com>
2016-10-14 15:41:26 +0200

    Fix UserScript headers

--- iitc-plugin-comm-scale-highlight.user.js
+++ iitc-plugin-comm-scale-highlight.user.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @id             iitc-plugin-comm-scale-highlight@phoudoin
 // @name           IITC plugin: links & fields scale in COMM
+// @category       Tweaks
 // @version        0.1.1.20161014.000002
 // @namespace      https://github.com/phoudoin/iitc-scale-highlight
 // @updateURL      https://github.com/phoudoin/iitc_plugins/raw/master/iitc-plugin-comm-scale-highlight.user.js
 // @downloadURL    https://github.com/phoudoin/iitc_plugins/raw/master/iitc-plugin-comm-scale-highlight.user.js
+// @description	   [phoudoin-2016-1014] colorized link length and field size in log according depending on their scale.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
 // @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
